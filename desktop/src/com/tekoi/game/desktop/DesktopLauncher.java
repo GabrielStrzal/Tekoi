67e39bfc08341ba3dee2c04e3a7cb7b4845c24ba
Gabriel Strzal <gabrielstrzal@gmail.com>
2018-06-23 18:31:44 -0300

    Project setup

---
+++ desktop/src/com/tekoi/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tekoi.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tekoi.game.TekoiGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TekoiGame(), config);
+	}
+}
---
+++ html/src/com/tekoi/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.tekoi.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.tekoi.game.TekoiGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TekoiGame();
+        }
+}
