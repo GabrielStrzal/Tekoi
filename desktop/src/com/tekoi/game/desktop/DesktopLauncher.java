f7a8ca0a919bbcf71ea3ee5477d56e47c2fe8afa
Gabriel Strzal <gabrielstrzal@gmail.com>
2018-06-26 20:44:24 -0300

    changed desktop launcher size

--- desktop/src/com/tekoi/game/desktop/DesktopLauncher.java
+++ desktop/src/com/tekoi/game/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package com.tekoi.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.tekoi.game.TekoiGame;
+import com.tekoi.game.config.GameConfig;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = (int) GameConfig.DISPLAY_SCREEN_WIDTH_PX;
+		config.height = (int)GameConfig.DISPLAY_SCREEN_HEIGHT_PX;
 		new LwjglApplication(new TekoiGame(), config);
 	}
 }
