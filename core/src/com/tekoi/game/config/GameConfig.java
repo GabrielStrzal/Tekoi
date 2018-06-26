1b06bf8ec6d659519141f7a1dddde76bfbeeed25
Gabriel Strzal <gabrielstrzal@gmail.com>
2018-06-25 22:52:05 -0300

    added gameconfig class

---
+++ core/src/com/tekoi/game/config/GameConfig.java
@@ -0,0 +1,21 @@
+package com.tekoi.game.config;
+
+
+public class GameConfig {
+
+    public static final float DISPLAY_SCREEN_WIDTH_PX = 800f; //pixels
+    public static final float DISPLAY_SCREEN_HEIGHT_PX = 480f; //pixels
+
+    public static final float DISPLAY_SCREEN_WIDTH_PX_HTML = 640f; //pixels
+    public static final float DISPLAY_SCREEN_HEIGHT_PX_HTML = 480f; //pixels
+
+
+    public static final float SCREEN_WIDTH_PX = 3200f; //pixels
+    public static final float SCREEN_HEIGHT_PX = 1920f; //pixels
+
+    public static final String GAME_VERSION = "V.0.0.1";
+
+    public static final boolean debug = false;
+
+    private GameConfig(){}
+}
