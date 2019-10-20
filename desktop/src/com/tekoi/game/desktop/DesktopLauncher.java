83aed59291d550a62776513321515e811e2b1737
Gabriel <gabrielstrzal@gmail.com>
2019-10-20 10:13:18 -0400

    changing the version of Lwjgl to 3

--- desktop/src/com/tekoi/game/desktop/DesktopLauncher.java
+++ desktop/src/com/tekoi/game/desktop/DesktopLauncher.java
@@ -1,15 +1,14 @@
 package com.tekoi.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.tekoi.game.TekoiGame;
 import com.tekoi.game.config.GameConfig;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = (int) GameConfig.SCREEN_WIDTH;
-		config.height = (int)GameConfig.SCREEN_HEIGHT;
-		new LwjglApplication(new TekoiGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration ();
+		config.setWindowedMode( (int)GameConfig.SCREEN_WIDTH, (int)GameConfig.SCREEN_HEIGHT);
+		new Lwjgl3Application(new TekoiGame(), config);
 	}
 }
